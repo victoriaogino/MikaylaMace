7619aab47a2f1b34a86729547884b9d7f01b9a88
Victoria Ogino <victoriaogino@gmail.com>
2020-09-05 20:47:35 -0700

    Updates

---
+++ precache-manifest.8a29ade635fd9d081412e7b6a62977f3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9a2188b9ce95392c2f17a1ceea7dbe",
+    "url": "/MikaylaMace/index.html"
+  },
+  {
+    "revision": "2a829e0546b09051bffe",
+    "url": "/MikaylaMace/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "f98a503133d407c70eda",
+    "url": "/MikaylaMace/static/css/main.6d2b1a71.chunk.css"
+  },
+  {
+    "revision": "2a829e0546b09051bffe",
+    "url": "/MikaylaMace/static/js/2.29dfaae3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MikaylaMace/static/js/2.29dfaae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f98a503133d407c70eda",
+    "url": "/MikaylaMace/static/js/main.5b621de2.chunk.js"
+  },
+  {
+    "revision": "f6b9b44239a5679fe173",
+    "url": "/MikaylaMace/static/js/runtime-main.d6f410d1.js"
+  },
+  {
+    "revision": "95e76a906bb753dbfd59e5ea85f58504",
+    "url": "/MikaylaMace/static/media/Mace_icon_1.95e76a90.png"
+  },
+  {
+    "revision": "790e205a81f4379033e66bad42be19b9",
+    "url": "/MikaylaMace/static/media/Mace_logo.790e205a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MikaylaMace/static/media/logo.5d5d9eef.svg"
+  }
+]);
